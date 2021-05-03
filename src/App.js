986c9addc12cd96cb93764281bed153fe661c61d
khw1031 <khw1031@gmail.com>
2021-05-03 09:17:58 +0900

    ssr2

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import About from "./About";
 import Home from "./Home";
+import DogeImg from "./doge_moon.jpeg";
 
 const Container = styled.div`
   background-color: #aaa;
-  border: 1px solid blue;
+  border: 1px solid red;
 `;
 
 export default function App({ page }) {
@@ -25,6 +26,7 @@ export default function App({ page }) {
   const PageComponent = pageState === "home" ? Home : About;
   return (
     <Container>
+      <img src={DogeImg} />
       <button data-page="home" onClick={onChangePage}>
         Home
       </button>
